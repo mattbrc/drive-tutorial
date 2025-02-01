337ede2c2192b3543aa4889fd3723efbba6e9884
Matt <director@acidgambit.com>
2025-01-31 19:01:32 -0800

    upd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,10 +14,7 @@ export default function HomePage() {
             target="_blank"
           >
             <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
+            <div className="text-lg">hello</div>
           </Link>
           <Link
             className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
